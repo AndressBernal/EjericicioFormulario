5909871a7ae58e5451105bdc92255cc10ad22207
AndressBernal <bernal20yl@gmail.com>
2023-11-13 09:29:50 -0500

    cambio

--- EjericicioUnoFormulario/Form1.Designer.cs
+++ EjericicioUnoFormulario/Form1.Designer.cs
@@ -62,9 +62,9 @@
             this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
             this.pictureBox1.ImageLocation = "System.Drawing.Bitmap";
             this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(602, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(468, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(198, 151);
+            this.pictureBox1.Size = new System.Drawing.Size(332, 318);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
